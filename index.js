10bd6bf22f504d11986fef58e6f9ab96e96194d7
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-01 19:56:39 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(names, target){
+    let matchedNames = names.filter(function (name){
+        return name.toLowerCase() === target.toLowerCase();
+    })
+    console.log(matchedNames);
+    return matchedNames;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+findMatching(drivers, "Bobby");
+
+function fuzzyMatch(names, target){
+    let matchedLetters = names.filter(function (name){
+        return name[0].toLowerCase() === target[0].toLowerCase();
+    })
+    console.log(matchedLetters);
+    return matchedLetters;
+}
+
+fuzzyMatch(drivers, "s");
+
+function matchName(names, target){
+    let matchedNames = names.filter(function (person){
+        return person.name.toLowerCase() === target.toLowerCase();
+    })
+    console.log(matchedNames);
+    return matchedNames;
+}
+
+matchName(drivers, "Bobby")
